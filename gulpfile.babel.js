ee2e1ce6c4398d7f3902e2a46b51a40e965c1d5d
Rob Fisher <me@robfisher.us>
2016-03-08 16:06:48 -0800

    Hopefully semi-final cleanup

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -150,6 +150,12 @@ gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
   return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
+gulp.task('deploy', ['build'], () => {
+  return gulp.src('dist')
+    .pipe($.subtree())
+    .pipe($.clean());
+});
+
 gulp.task('default', ['clean'], () => {
   gulp.start('build');
 });
